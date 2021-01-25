a567048e2adbfdce637afacbb6e40ba92dc5fdd8
Fabio <fabio.rdrgs@eduge>
2021-01-25 11:06:49 +0100

    NavBar + TextArea et FileSelect

---
+++ Home/index.php
@@ -0,0 +1,75 @@
+<?php
+session_start();
+$_SESSION['currentPage'] = "Home";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <script src="https://kit.fontawesome.com/b028745828.js" crossorigin="anonymous"></script>
+  <title>The Zone - MonEspace</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/blog-home.css" rel="stylesheet">
+
+</head>
+
+<body>
+  <!--#region Navigation  -->
+
+  <!-- Navigation -->
+<?php include_once "../php/navbar.php"?>
+ 
+  <!--#endregion -->
+ 
+  <!-- Page Content -->
+
+  <div class="container">
+  
+      <!-- Sidebar Widgets Column -->
+      <div class="col-md-4">
+
+        <!-- Profile Info -->
+       <div class="card my-4">
+          <div class="card-header">
+            <img src="../img/Ville.png" alt="Image du Blog" class="card-img-top">
+            <h6 class="card-text">Bienvenue sur MonEspace</h6>
+          </div>        
+
+          <div class="card-body">
+            <h6 class="card-title">F. Santos</h6>
+            <p class="card-text">239 Followers, 0 Posts</p>
+           <img src="../img/NoPFP.jpg" alt="Image de Profile" class=" card-img-bottom" style="width:20%">
+          
+          </div>
+       </div>
+
+      
+      
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; TheZone</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
---
+++ Post/index.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+$_SESSION['currentPage'] =  "Post";
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <script src="https://kit.fontawesome.com/b028745828.js" crossorigin="anonymous"></script>
+  <title>The Zone - MonEspace</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/blog-post.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!--#region Navigation  -->
+  <!-- Navigation -->
+  <?php include_once "../php/navbar.php"?>
+  <!--#endregion -->
+<form action="./index.php" enctype="multipart/form-data"> 
+
+<label for="postTextArea">Entrez du text</label></br>
+<textarea id="postTextArea" rows="20" cols="50"></textarea></br>
+<label for="fileSelect"> Select a file:</label> <input id="fileSelect" type="file" name="img[]" multiple>
+<input type="submit">
+</form>
+
+<?php var_dump($_FILES);
+
+var_dump($_GET['img']);?>
+
+ <!-- Footer -->
+ <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; TheZone</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
