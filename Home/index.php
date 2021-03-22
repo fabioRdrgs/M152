93200219a5628f6a7f51c0783bd0130a9d52b011
Fabio <fabio.rdrgs@eduge>
2021-03-22 08:56:22 +0100

    Correction index inexistant affichage posts

--- Home/index.php
+++ Home/index.php
@@ -1,7 +1,9 @@
-<?php 
+<?php
 require_once '../php/user_show_post.inc.php';
 $arrayPosts = show_all_Posts();
-$mediaPost =[];
+//Ajout d'un post null afin d'empêcher qu'un index inexistant soit testé lors de l'affichage des posts
+array_push($arrayPosts,null);
+$mediaPost = [];
 $postCount;
 if (!isset($_SESSION))
   session_start();
@@ -28,7 +30,7 @@ $_SESSION['currentPage'] = "Home";
 </head>
 
 <body>
- <!-- Template Post
+  <!-- Template Post
         <div class="card mb-4">
           <img class="card-img-top" src="http://placehold.it/750x300" alt="Card image cap">
           <div class="card-body">
@@ -63,114 +65,116 @@ $_SESSION['currentPage'] = "Home";
           </div>
           <div class="card-body">
             <h6 class="card-title">F. Santos</h6>
-            <p class="card-text">239 Followers, <?=count($arrayPosts) ?> posts</p>
+            <p class="card-text">239 Followers, <?php if (isset($arrayPosts)) echo count($arrayPosts)-1; ?> posts</p>
             <img src="../img/NoPFP.jpg" alt="Image de Profile" class=" card-img-bottom" style="width:20%">
           </div>
         </div>
-      </div> 
- 
-    
-      <!-- Post Column -->
-    <div class="col-md-8">
+      </div>
 
-      <?php
+
+      <!-- Post Column -->
+      <div class="col-md-8">
+
+        <?php
         //For permettant d'afficher chaque posts
-      for($i = 1; $i <= count($arrayPosts);$i++)
-      {
-        if($arrayPosts[$i-1]['nomMedia'] != null)
-        {  
-          //Si l'id actuel est égal à l'id précédant, insère le nom de l'image ainsi que son extension dans un array pour utilisation future
-            if($arrayPosts[$i-1]['idPost'] == $arrayPosts[$i]['idPost'])
+        for ($i = 1; $i < count($arrayPosts) ; $i++) 
+        {
+          if ($arrayPosts[$i - 1]['nomMedia'] != null) 
+          {
+            //Si l'id actuel est égal à l'id précédant, insère le nom de l'image ainsi que son extension dans un array pour utilisation future
+            if ( $i == count($arrayPosts) || $arrayPosts[$i - 1]['idPost'] == $arrayPosts[$i]['idPost']) 
             {
-              array_push($mediaPost,[$arrayPosts[$i-1]['nomMedia'],$arrayPosts[$i-1]['extMedia']]);                   
+              array_push($mediaPost, [$arrayPosts[$i - 1]['nomMedia'], $arrayPosts[$i - 1]['extMedia']]);
             }
             //Lorsque l'id n'est plus le même, cela veut dire qu'il y a un autre post actuellement. Donc nous allons ajouter la dernière image à l'array
             //Précédement crée et afficher le post précédant avant de vider l'array pour recommencer l'opération autant de fois que le for se lance qui est
             //égal aux d'images au total pour tous les posts
-            else
+            else 
             {
-              array_push($mediaPost,[$arrayPosts[$i-1]['nomMedia'],$arrayPosts[$i-1]['extMedia']]); 
+              array_push($mediaPost, [$arrayPosts[$i - 1]['nomMedia'], $arrayPosts[$i - 1]['extMedia']]);
 
               //Affiche le post
               echo
               "<div class=\"card mb-4\"> ";
-                    //Affiche chaque images
-                    foreach($mediaPost as $media)
-                    {              
-                      if($media[1] == "mp4" || $media[1] == "ogg" || $media[1] == "webm")
-                      echo "
+              //Affiche chaque images
+              foreach ($mediaPost as $media) 
+              {
+                if ($media[1] == "mp4" || $media[1] == "ogg" || $media[1] == "webm")
+                  echo "
                       <video autoplay controls loop>
-                      <source  src=\"../tmp/".$media[0].".".$media[1]."\" type=\"video/mp4\">
-                      <source  src=\"../tmp/".$media[0].".".$media[1]."\" type=\"video/ogg\">
-                      <source  src=\"../tmp/".$media[0].".".$media[1]."\" type=\"video/webm\">
+                      <source  src=\"../tmp/" . $media[0] . "." . $media[1] . "\" type=\"video/mp4\">
+                      <source  src=\"../tmp/" . $media[0] . "." . $media[1] . "\" type=\"video/ogg\">
+                      <source  src=\"../tmp/" . $media[0] . "." . $media[1] . "\" type=\"video/webm\">
                       Your browser does not support the video tag.
                       </video>";
-                      else if ($media[1] == "mpeg" || $media[1] == "ogg")
-                      {
-                          echo "
+                else if ($media[1] == "mpeg" || $media[1] == "ogg") 
+                {
+                  echo "
                           <audio autostart=\"false\" controls>
-                          <source  src=\"../tmp/".$media[0].".".$media[1]."\" type=\"audio/ogg\">
-                          <source  src=\"../tmp/".$media[0].".".$media[1]."\" type=\"video/mpeg\">
+                          <source  src=\"../tmp/" . $media[0] . "." . $media[1] . "\" type=\"audio/ogg\">
+                          <source  src=\"../tmp/" . $media[0] . "." . $media[1] . "\" type=\"video/mpeg\">
                           </audio>";
-                      }
-                      else
-                    echo "<img style=\"width:300px;height:250px;\"class=\"card-img-top\" src=\"../tmp/".$media[0].".".$media[1]."\" alt=\"Card image cap\">";
-                    }
-                    
-                  echo " <div class=\"card-body\"> 
+                } else
+                  echo "<img style=\"width:300px;height:250px;\"class=\"card-img-top\" src=\"../tmp/" . $media[0] . "." . $media[1] . "\" alt=\"Card image cap\">";
+              }
+
+              
+              
+
+              echo " <div class=\"card-body\"> 
                     <p class=\"card-text\">";
-                    //Affiche le commentaire
-                    echo $arrayPosts[$i-1]['postCommentaire'];
-                    echo "</p>
-                      <a href=\"../Post/index.php?idPost=".$arrayPosts[$i-1]['idPost']."\" class=\"btn btn-primary\">Modifier le post</a> 
-                      <a href=\"../php/deleteFile.php?idPost=". $arrayPosts[$i-1]['idPost']."\" class=\"btn btn-primary\">Supprimer le post</a>
+              //Affiche le commentaire
+              echo $arrayPosts[$i - 1]['postCommentaire'];
+              echo "</p>
+                      <a href=\"../Post/index.php?idPost=" . $arrayPosts[$i - 1]['idPost'] . "\" class=\"btn btn-primary\">Modifier le post</a> 
+                      <a href=\"../php/deleteFile.php?idPost=" . $arrayPosts[$i - 1]['idPost'] . "\" class=\"btn btn-primary\">Supprimer le post</a>
                       </div> 
                       <div class=\"card-footer text-muted\"> Posté le : ";
-                      //Affiche la date de création du post
-                      $dateCreationPost =  explode(" ",$arrayPosts[$i-1]['postDateCreation']);
-                      echo $dateCreationPost[0]. " à ". $dateCreationPost[1];
+              //Affiche la date de création du post
+              $dateCreationPost =  explode(" ", $arrayPosts[$i - 1]['postDateCreation']);
+              echo $dateCreationPost[0] . " à " . $dateCreationPost[1];
 
-                      echo " by F. Santos</div></div>"; 
-              
-                $mediaPost = [];         
-            }     
-          }
-          else if($arrayPosts[$i-1]['nomMedia'] == null) 
-          {     
-          echo "<div class=\"card mb-4\"> ";
-          echo " <div class=\"card-body\"> 
+              echo " by F. Santos</div></div>";
+
+              $mediaPost = [];
+            }
+          } 
+          else if ($arrayPosts[$i - 1]['nomMedia'] == null)
+          {
+            echo "<div class=\"card mb-4\"> ";
+            echo " <div class=\"card-body\"> 
                     <p class=\"card-text\">";
-                    //Affiche le commentaire
-                    echo $arrayPosts[$i-1]['postCommentaire'];
-                    echo "</p>
-                    <a href=\"../Post/index.php?idPost=".$arrayPosts[$i-1]['idPost']."\" class=\"btn btn-primary\">Modifier le post</a> 
-                    <a href=\"../php/deleteFile.php?idPost=". $arrayPosts[$i-1]['idPost']."\" class=\"btn btn-primary\">Supprimer le post</a>
+            //Affiche le commentaire
+            echo $arrayPosts[$i - 1]['postCommentaire'];
+            echo "</p>
+                    <a href=\"../Post/index.php?idPost=" . $arrayPosts[$i - 1]['idPost'] . "\" class=\"btn btn-primary\">Modifier le post</a> 
+                    <a href=\"../php/deleteFile.php?idPost=" . $arrayPosts[$i - 1]['idPost'] . "\" class=\"btn btn-primary\">Supprimer le post</a>
                 </div> 
                       <div class=\"card-footer text-muted\"> Posté le : ";
-                      //Affiche la date de création du post
-                      $dateCreationPost =  explode(" ",$arrayPosts[$i-1]['postDateCreation']);
-                      echo $dateCreationPost[0]. " à ". $dateCreationPost[1];
+            //Affiche la date de création du post
+            $dateCreationPost =  explode(" ", $arrayPosts[$i - 1]['postDateCreation']);
+            echo $dateCreationPost[0] . " à " . $dateCreationPost[1];
 
-                      echo " by F. Santos</div></div>";
+            echo " by F. Santos</div></div>";
           }
         }
-        
-          
-      ?>
+
+
+        ?>
+      </div>
     </div>
+    <!-- /.container -->
   </div>
-        <!-- /.container -->
-    </div>
   <!-- Footer -->
   <footer class="py-5 bg-dark">
-      <div class="container">
-        <p class="m-0 text-center text-white">Copyright &copy; TheZone</p>
-      </div>
-    </footer>
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; TheZone</p>
+    </div>
+  </footer>
 
-    <!-- Bootstrap core JavaScript -->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 </body>
 
